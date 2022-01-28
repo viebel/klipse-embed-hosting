6578621be2661e20cffe420417bb9c2428c0b9bb
Yehonathan Sharvit <viebel@gmail.com>
2022-01-28 06:50:48 +0200

    Social media images

--- web/index.php
+++ web/index.php
@@ -1,4 +1,7 @@
-<?php $klipse_embed_url = 'https://viebel.github.io/klipse-embed' ?>
+<?php 
+  $klipse_embed_url = 'https://viebel.github.io/klipse-embed';
+  $screenshot_url = $klipse_embed_url . "/img/klipse_snippet.png?url=" . urlencode("https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
+?>
 <!doctype html>
 <html lang="en" translate="no">
 <head>
@@ -9,15 +12,15 @@
     <meta name="keywords" content="Klipse">
     <meta property="og:title" content="Klipse Interactive and Shareable Code Snippet">
     <meta property="og:description" content="Klipse Interactive and Shareable Code Snippet">
-    <meta property="og:image" content='https://orgpad.com/screenshot?url=<?php echo urlencode("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]")?>'>
+    <meta property="og:image" content="<?php echo $screenshot_url ?>">
     <meta property="og:site_name" content="Klipse Embed">
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:site" content="@viebel">
     <meta name="twitter:title" content="Klipse Interactive and Shareable Code Snippet">
     <meta name="twitter:description" content="Klipse Interactive and Shareable Code Snippet">
-    <meta property="twitter:image" content='<?php echo $klipse_embed_url ?>/img/klipse_snippet.png?url=<?php echo urlencode("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]")?>'>
+    <meta property="twitter:image" content="<?php echo $screenshot_url ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<link rel="icon" href="<?php echo $klipse_embed_url ?>/img/favicon.png">
+    <link rel="icon" href="<?php echo $klipse_embed_url ?>/img/favicon.png">
 </head>
 <body>
     <div id="snippets"></div>
