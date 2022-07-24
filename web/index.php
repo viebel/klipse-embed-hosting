aa942e9c21978b41d5eb86290f12bc5a76646da2
Yehonathan Sharvit <viebel@Yehonathans.local>
2022-07-24 04:25:48 +0300

    600x600

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php 
-  $screenshot_url = "https://api.urlbox.io/v1/JejmqldbRkoarwiU/jpg?width=300&height=300&wait_until=requestsfinished&url=" . urlencode("https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
+  $screenshot_url = "https://api.urlbox.io/v1/JejmqldbRkoarwiU/jpg?width=600&height=600&wait_until=requestsfinished&url=" . urlencode("https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
 ?>
 <!doctype html>
 <html lang="en" translate="no">
